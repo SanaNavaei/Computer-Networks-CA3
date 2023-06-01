c1801bb80c3b1161ded3d804e92041bd8390dd38
SanaNavaei <sana.sarinavaei@gmail.com>
2023-06-01 16:17:49 +0330

    Add some functions for dvrp in headerfile

--- include/graph.hpp
+++ include/graph.hpp
@@ -7,11 +7,12 @@
 #include <vector>
 #include <set>
 #include <cstring>
+#include <climits>
 
 #define SPACE ' '
 #define DELIMITER '-'
+#define INFINITY 1e9
 
-#define ERR_SAMENODES "Error: u and v are the same."
 #define EMPTY_GRAPH "The Adjacency Matrix is empty."
 #define ERROR "Error"
 
@@ -27,9 +28,13 @@ private:
 
 public:
     void create_graph(std::string args);
+    void print_handler(std::string word, int space_size);
     void show();
     void modify(std::string args);
     void remove(std::string args);
+    void handler_dvrp(std::string args);
+    void dvrp(int source);
+    void print_dvrp(int source, std::vector<int> &distance, std::vector<int> &parent);
 
     bool handle_same_nodes(int u, int v);
 
